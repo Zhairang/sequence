d1d4967f2f5da2e95070c39d5e8a4a0f4b96b0c3
JinYonghao <jyh1@mail.ustc.edu.cn>
2015-07-20 11:55:32 +0800

    ver 0.11

--- main.hs
+++ main.hs
@@ -2,16 +2,18 @@ import Command
 
 import Sequence
 
-welcome = "Hello!\n"
+welcome = "Hello!"
 
 main = do
   putStrLn welcome
+  putStrLn divider
   mainloop
 
 mainloop = do
   input <- getLine
   modified <- eval (read input)
   putStrLn (show modified)
+  putStrLn divider
   mainloop
   
-  
+divider = take 60 (repeat '*')
